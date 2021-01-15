0cfd8c4df57cc4b20e631a8151b13da170875553
dimatkach11 <dimitritkach@gmail.com>
2021-01-15 16:26:21 +0100

    tolto alcune funzioni di default ed eliminato un file

--- wp-content/themes/tonik/resources/templates/index.tpl.php
+++ wp-content/themes/tonik/resources/templates/index.tpl.php
@@ -1,13 +1,5 @@
 <?php get_header(); ?>
 
-<?php
-    /**
-     * Functions hooked into `theme/index/header` action.
-     *
-     * @hooked Tonik\Theme\Index\render_header - 10
-     */
-    do_action('theme/index/header');
-?>
 
 <section class="section">
     <div class="wrapper">
